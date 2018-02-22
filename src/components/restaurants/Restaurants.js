52983a4a5bda96f2f878d03ab51b3892872a2f1a
L-A-Davis <lindsay.amanda.davis@gmail.com>
2018-02-21 22:37:45 -0500

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
   render() {
+    const restaurants =
+    this.props.store.getState().restaurants.map((restaurant , index) => {
+      return <Restaurant restaurant={restaurant} key={index} store={this.props.store} />
+    })
     return(
       <ul>
-        Restaurants Component
+        {restaurants}
       </ul>
     );
   }
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
-  render() {
-    return (
-      <ul>
-        Reviews
-      </ul>
-    );
-  }
-};
+
+    render() {
+      const { store, restaurantId } = this.props;
+      const associatedReviews = store.getState().reviews.filter(review => review.restaurantId === restaurantId);
+      const reviews = associatedReviews.map((review, index) => {
+        return <Review store={store} key={index} review={review} />
+      })
+
+      return (
+        <div>
+          <ul>
+            {reviews}
+          </ul>
+        </div>
+      );
+    }
+
+  };
 
 export default Reviews;
